2335d121256eae79553191e6f18b52534d0c6c99
Santiago <santidotpy@gmail.com>
2025-10-21 16:09:44 -0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,18 @@
+import { nextCookies } from 'better-auth/next-js';
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@better-t-movies/db";
+import * as schema from "@better-t-movies/db/schema/auth";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+  plugins: [nextCookies()]
+});
