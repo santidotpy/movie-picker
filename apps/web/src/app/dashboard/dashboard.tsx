4bfe6befa026d253109e2362d8dd0b79f4ff8464
Santiago <santidotpy@gmail.com>
2025-10-24 00:25:08 -0300

    format code

--- apps/web/src/app/dashboard/dashboard.tsx
+++ apps/web/src/app/dashboard/dashboard.tsx
@@ -4,16 +4,17 @@ import { authClient } from "@/lib/auth-client";
 import { useQuery } from "@tanstack/react-query";
 import { trpc } from "@/utils/trpc";
 
+// TODO: Bento Grid con algunas estadisticas de las listas
 export default function Dashboard({
-	session,
+  session,
 }: {
-	session: typeof authClient.$Infer.Session;
+  session: typeof authClient.$Infer.Session;
 }) {
-	const privateData = useQuery(trpc.privateData.queryOptions());
+  const privateData = useQuery(trpc.privateData.queryOptions());
 
-	return (
-		<>
-			<p>API: {privateData.data?.message}</p>
-		</>
-	);
+  return (
+    <>
+      <p>API: {privateData.data?.message}</p>
+    </>
+  );
 }
